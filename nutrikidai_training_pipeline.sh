8f640314f0bee4fa591b3a07c60d6a0041183244
Frederick Gyasi <gyasifred@gmail.com>
2025-04-19 15:28:10 -0400

    Update nutrikidai_training_pipeline.sh

--- nutrikidai_training_pipeline.sh
+++ nutrikidai_training_pipeline.sh
@@ -51,11 +51,11 @@ TABPFN_OUTPUT_DIR="${OUTPUT_BASE_DIR}/TabPFN"
 export RAY_FUNCTION_SIZE_ERROR_THRESHOLD=200
 # LLM fine-tuning settings
 LLM_MODELS=(
-   # "unsloth/Llama-3.2-1B-Instruct-bnb-4bit"
-   #  "unsloth/gemma-7b-it-bnb-4bit"
-   # "unsloth/Phi-4"
-   "unsloth/Meta-Llama-3.1-8B-Instruct-bnb-4bit"
-   "unsloth/mistral-7b-instruct-v0.2-bnb-4bit"
+   "unsloth/Llama-3.2-1B-Instruct-bnb-4bit"
+    "unsloth/gemma-7b-it-bnb-4bit"
+   "unsloth/Phi-4"
+   # "unsloth/Meta-Llama-3.1-8B-Instruct-bnb-4bit"
+   # "unsloth/mistral-7b-instruct-v0.2-bnb-4bit"
    )
 LLM_BASE_DIR="${OUTPUT_BASE_DIR}/LLM_final"
 
