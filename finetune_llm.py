687d14df89e6abaf38aaeb335631bf5a1f141bf4
Frederick Gyasi <gyasifred@gmail.com>
2025-04-19 02:04:18 -0400

    Update finetune_llm.py

--- finetune_llm.py
+++ finetune_llm.py
@@ -57,7 +57,7 @@ def parse_arguments():
                         help="Per-device training batch size")
     parser.add_argument("--learning_rate", type=float, default=2e-4,
                         help="Learning rate for training")
-    parser.add_argument("--max_seq_length", type=int, default=4096,
+    parser.add_argument("--max_seq_length", type=int, default=2048,
                         help="Override model's default maximum sequence length (optional)")
     parser.add_argument("--epochs", type=int, default=1,
                         help="Number of training epochs")
